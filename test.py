48399c524f5a3c8c5819c39b91ddfcdc6e0eb618
Ericfengdc <43702827+Ericfengdc@users.noreply.github.com>
2018-11-15 13:32:24 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+
+import json
+# json_text=open('j',encoding='UTF-8').read()
+# #load需要的打开文件但是python3目前不支持file，不知道怎么搞成文件类型
+# # f=json.load(open('j'))
+#
+# # f=json.loads('{"qew":12,"asdad":23}')
+# f=json.loads(json_text)
+# print(f["qew"])
+with open ("test.txt",'w')as f:
+    f.write('in哦i好i是多少')
